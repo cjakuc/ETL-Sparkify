5f48fd51536d9f6c704ed4c1b8b6062602370a28
cjakuc <chris.jakuc@gmail.com>
2021-03-21 20:27:28 -0400

    finished etl.ipynb

--- sql_queries.py
+++ sql_queries.py
@@ -9,7 +9,7 @@ time_table_drop = "DROP TABLE IF EXISTS time"
 # CREATE TABLES
 
 songplay_table_create = ("""
-CREATE TABLE IF NOT EXISTS songplays(songplay_id varchar PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS songplays(songplay_id SERIAL PRIMARY KEY,
                                      start_time timestamp,
                                      user_id int,
                                      level varchar,
@@ -111,13 +111,14 @@ ADD CONSTRAINT
 # INSERT RECORDS
 
 songplay_table_insert = ("""
-INSERT INTO songplays (songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
-VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
+VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
 """)
 
 user_table_insert = ("""
 INSERT INTO users (user_id, first_name, last_name, gender, level)
 VALUES(%s, %s, %s, %s, %s)
+ON CONFLICT (user_id) DO NOTHING
 """)
 
 song_table_insert = ("""
@@ -128,6 +129,7 @@ VALUES (%s, %s, %s, %s, %s)
 artist_table_insert = ("""
 INSERT INTO artists (artist_id, name, location, latitude, longitude)
 VALUES (%s, %s, %s, %s, %s)
+ON CONFLICT (artist_id) DO NOTHING
 """)
 
 
@@ -139,6 +141,16 @@ VALUES (%s, %s, %s, %s, %s, %s, %s)
 # FIND SONGS
 
 song_select = ("""
+SELECT
+    songs.song_id,
+    songs.artist_id
+FROM
+    songs
+INNER JOIN artists ON artists.artist_id = songs.artist_id
+WHERE
+    songs.title = %s
+    AND artists.name = %s
+    AND songs.duration = %s
 """)
 
 # QUERY LISTS
